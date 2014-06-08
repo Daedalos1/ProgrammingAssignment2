1b9be03b0bb8d2b1ed9b0bfe3f568b6af3c86bb4
Daedalos1 <ianhagan@tpg.com.au>
2014-06-08 17:52:04 +1000

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,7 +2,7 @@
 ## functions do
 
 ## Write a short comment describing this function
-##Testing ability to edit
+##Testing ability to edit and paste
 makeCacheMatrix <- function(x = matrix()) {
 
 }
