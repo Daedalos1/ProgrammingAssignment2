2acc087da7ab768cbe0c222c4bc06651a0448892
Daedalos1 <ianhagan@tpg.com.au>
2014-06-15 20:54:57 +1000

    testing commit to github

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,21 @@
 ## Put comments here that give an overall description of what your
 ## functions do
-##Just trying out editing
-## Write a short comment describing this function
-##Testing ability to edit and paste
-makeCacheMatrix <- function(x = matrix()) {
 
+## Write a short comment describing this function
+
+makeCacheMatrix <- function(x = matrix()) {
+        m <-NULL
+        set<-function(y){
+                x <<- y
+                m <<- NULL
+        }
+        get<-function() x
+        setmatrix <- function(solve) m <<- solve
+        getmatrix <-function() m
+        list(set = set, get = get,
+             setmatrix = setmatrix,
+             getmatrix = getmatrix)
+        
 }
 
 
@@ -12,4 +23,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <-x$getmatrix
+        if(!is.null(m)){
+                message("getting cached data")
+                return(m)
+        }
+        matrix <- x$get()
+        m <- solve(matrix, ...)
+        x$setmatrix(m)
+        m
+        
 }
